4ef8ed7dee76dc8d4a22e320d215f85c261be64e
johannessorg <johannes-sorg@outlook.de>
2018-06-14 19:53:21 +0200

    newest version

--- ios/testproj/AppDelegate.m
+++ ios/testproj/AppDelegate.m
@@ -11,12 +11,24 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "MoPub.h"
+#if RCT_DEV
+#import <React/RCTDevLoadingView.h>
+#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+  
+  MPMoPubConfiguration * sdkConfig = [[MPMoPubConfiguration alloc] initWithAdUnitIdForAppInitialization: @"0ac59b0996d947309c33f59d6676399f"];
+  sdkConfig.globalMediationSettings = @[];
+  sdkConfig.mediatedNetworks = @[];
+  sdkConfig.advancedBidders = nil;
+  [[MoPub sharedInstance] initializeSdkWithConfiguration:sdkConfig completion:^{
+    NSLog(@"SDK initialization complete");
+  }];
 
   #ifdef DEBUG
     jsCodeLocation = [NSURL URLWithString:@"http://192.168.178.3:8081/index.bundle?platform=ios&dev=true"];;
@@ -24,10 +36,16 @@
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   #endif
 
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"testproj"
-                                               initialProperties:nil
-                                                   launchOptions:launchOptions];
+  RCTBridge *bridge = [[RCTBridge alloc] initWithBundleURL:jsCodeLocation
+                                            moduleProvider:nil
+                                             launchOptions:launchOptions];
+  #if RCT_DEV
+    [bridge moduleForClass:[RCTDevLoadingView class]];
+  #endif
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                moduleName:@"testproj"
+                                                initialProperties:nil];
+
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
